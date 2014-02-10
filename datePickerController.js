cb0f0864a4b78a40f0fe1763f15806beab586b5e
torkieri <torkieri@gmail.com>
2014-02-10 11:20:51 +0100

    testtest
    test

---
+++ datePickerController.js
@@ -0,0 +1,43 @@
+﻿//(function (module) {
+//    'use strict';
+
+//    module.controller("DatePickerController",
+//        [
+//            "$scope", "$timeout",
+            function DatePickerController ($scope, $timeout) {
+
+                // Disable weekend selection
+                $scope.disabled = function (date, mode) {
+                    return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 7));
+                };
+
+                $scope.routineMinDate = Date.today();
+                //$scope.routineMaxDate = $scope.model.deadline || new Date($scope.routineMinDate).addYears(2);
+                $scope.routineMaxDate = new Date($scope.routineMinDate).addYears(2);
+
+                $scope.completionMinDate = Date.today();
+                $scope.completionMaxDate = new Date($scope.completionMinDate).addYears(2);
+
+                $scope.openRoutineDeadline = function () {
+                    $scope.openedRoutineDeadline = true;
+                };
+
+                $scope.openCompletionDeadline = function ($event) {
+
+                    //if ($scope.model.routineDeadline)
+                    //    $scope.completionMinDate = $scope.model.routineDeadline;
+                    $timeout(function () {
+                        $event.preventDefault();
+                        $event.stopPropagation();
+                        $scope.openedCompletionDeadline = true;
+                    });
+                };
+
+                $scope.dateOptions = {
+                    'year-format': "'yy'",
+                    'starting-day': 1
+                };
+
+            }
+//            ]);
+//}(angular.module("app")));
